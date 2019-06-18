bf9ad4d7112545316eecf8a5540d38c22060780c
David Kavanaugh <davidkavanaugh@outlook.com>
2019-06-18 06:28:26 -0700

    copyToClipboard

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -10,7 +10,29 @@ class Home extends Component {
       for (let [key, value] of Object.entries(values)) {
         this.selections.push(key + ': ' + value)
       }
-      console.log(this.selections);
+      let toCopy = this.selections.join('\n').toString();
+
+      // Create a dummy input to copy the string array inside it
+      let copyBox = document.createElement("textarea");
+
+      // Add it to the document
+      document.body.appendChild(copyBox);
+
+      // Set its ID
+      copyBox.setAttribute("id", "copy-selections");
+      
+      // Output the array into it
+      document.getElementById("copy-selections").value=toCopy;
+
+      // Select it
+      copyBox.select();
+
+      // Copy its contents
+      document.execCommand("copy");
+
+      // Remove it as its not needed anymore
+      document.body.removeChild(copyBox);
+
       this.selections = [];
     };
 
