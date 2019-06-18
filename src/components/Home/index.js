a5656417e5b603308f23005b87912ef3c07de05b
David Kavanaugh <davidkavanaugh@outlook.com>
2019-06-18 06:00:49 -0700

    add to selections

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,9 +3,16 @@ import Form from './Form';
 import { connect } from 'react-redux';
 
 class Home extends Component {
-    submit = values => {
-        console.log(values);
-      }
+  selections =[];
+
+  submit = values => {
+    for (let [key, value] of Object.entries(values)) {
+      this.selections.push(`${key}: ${value}`);
+      console.log(this.selections);
+      this.selections = [];
+    }
+    
+  }
     render() { 
         return ( 
                 <Form onSubmit={this.submit} questions={this.props.questions} />
